d96274d2a20daa13f66e91d6b4214cc44ac9b4a7
Emad <emadmakhlouf12@gmail.com>
2025-04-22 15:43:12 +0000

    Added mono summing mechanism. Added a resampling function for to resample audio to 16KHz. Refactored the code for modularity.

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,83 @@
 use hound::WavReader;
-use std::ffi::CStr;
+use std::{ffi::CStr, i16};
 use std::os::raw::c_char;
+use rubato::{SincFixedIn, SincInterpolationType, SincInterpolationParameters, WindowFunction, Resampler};
 
-#[no_mangle]
-pub extern "C" fn read_wav_info(path: *const c_char) {
-    let c_str = unsafe { CStr::from_ptr(path) };
-    let path_str = match c_str.to_str() {
-        Ok(s) => s,
-        Err(_) => {
-            eprintln!("Invalid path string");
-            return;
-        }
-    };
-
-    let reader = match WavReader::open(path_str) {
-        Ok(r) => r,
-        Err(e) => {
-            eprintln!("Failed to read wav file: {}", e);
-            return
-        }
-    };
-
+fn read_wav(path: &str) -> Result<(Vec<f32>, u32), String> {
+    let reader = WavReader::open(path)
+        .map_err(|e| format!("Failed to open WAV: {}", e))?;
     let spec = reader.spec();
-    println!("Sampling rate:  {}", spec.sample_rate);
-    println!("Bits per sampmle: {}", spec.bits_per_sample);
-    println!("Num channels: {}", spec.channels);
-
+    let orig_sample_rate = spec.sample_rate;
     let samples: Vec<f32> = reader
         .into_samples::<i16>()
         .filter_map(Result::ok)
         .map(|s| s as f32 / i16::MAX as f32)
         .collect();
 
-    println!("Loaded {} samples", samples.len());
+    let mono = if spec.channels == 1 {
+        samples
+    } else {
+        samples
+            .chunks(spec.channels as usize)
+            .map(|chunk| chunk.iter().sum::<f32>() / chunk.len() as f32)
+            .collect()
+    };
+
+    Ok((mono, orig_sample_rate))
 }
+
+fn resample_audio(input: Vec<f32>, orig_rate: u32, target_rate: u32) -> Result<Vec<f32>, String> {
+    if orig_rate == target_rate {
+        return Ok(input)
+    }
+
+    let ratio = target_rate as f64 / orig_rate as f64;
+    let params = SincInterpolationParameters {
+        sinc_len: 256,
+        f_cutoff: 0.95,
+        oversampling_factor: 256,
+        interpolation: SincInterpolationType::Cubic,
+        window: WindowFunction::BlackmanHarris2,
+    };
+    let mut resampler = SincFixedIn::<f32>::new(ratio, 1.0, params, input.len(), 1)
+        .map_err(|e| format!("Resampler init error: {:?}", e))?;
+    let outputs = resampler
+        .process(&[input], None)
+        .map_err(|e| format!("Resampler init error: {:?}", e))?;
+    Ok(outputs.into_iter().next().unwrap())
+}
+
+
+#[no_mangle]
+pub extern "C" fn extract_whisper_features(path: *const c_char) {
+    let c_str = unsafe { CStr::from_ptr(path) };
+    let path_str = match c_str.to_str() {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!("Invalid UTF-8 path");
+            return;
+        }
+    };
+
+    let (mono, orig_sample_rate) = match read_wav(path_str) {
+        Ok(v) => v,
+        Err(err) => {
+            eprint!("{}", err);
+            return;
+        }
+    };
+
+    let mel_ready = match resample_audio(mono, orig_sample_rate, 16000) {
+        Ok(buf) => buf,
+        Err(err) => {
+            eprintln!("{}", err);
+            return;
+        }
+    };
+
+    println!("Final sample count: {}", mel_ready.len())
+}
+
+
+
+
